4fd2cc40e71231d7d0cb12ae98a602944dd6db78
Mateusz Marmuźniak <4PTP_2@DESKTOP-HMD73UL>
2023-12-13 10:14:53 +0100

    Add class ChartData

--- GitMarmuzniak/GitMarmuzniak/MainPage.xaml.cs
+++ GitMarmuzniak/GitMarmuzniak/MainPage.xaml.cs
@@ -8,11 +8,29 @@ using Xamarin.Forms;
 
 namespace GitMarmuzniak
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
+        public static List<ChartData> ChartData {  get; set; }
         public MainPage()
         {
             InitializeComponent();
+            ChartData = new List<ChartData>
+            {
+                new ChartData("Seria 1", 15),
+                new ChartData("Seria 2", 35),
+                new ChartData("Seria 3", 5),
+                new ChartData("Seria 4", 75),
+            };
+        }
+
+        private void WykresSlupkowy_Appearing(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WykresKolowy_Appearing(object sender, EventArgs e)
+        {
+
         }
     }
 }
